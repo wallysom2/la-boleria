4debc962ad3c79830a83726776ff6b24c7621ed0
matheus <wallysom2@gmail.com>
2022-10-25 12:57:36 -0300

    config: alteracoes para deploy no heroku

--- src/index.js
+++ src/index.js
@@ -13,6 +13,6 @@ app.use(cors());
 
 app.use (router)
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port " + process.env.PORT));
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
